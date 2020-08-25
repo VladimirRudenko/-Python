faf7a2d7065f9a5b4c687733d6d7c88f282f4487
Vladimir <vladimir1rudenko@gmail.com>
2020-08-25 18:58:54 +0300

    fearst lesson

---
+++ 1.py
@@ -0,0 +1,25 @@
+s = 25
+print("this is integer ->" ,s)
+
+lst = [7, "f", "ds", "ds"]
+print ("this is list ->" + str(lst) )
+
+print ("this is String -> string")
+
+r1 = {'model': '4451', 'ios': '15.4'}
+print("this is dictinary-> " + str(r1))
+
+set = (11, 22, 44, 33)
+print("this is set -> " + str(set))
+
+tuple1 = ("bla", "bla2", "bla3")
+print ("this is tuple -> " + str(tuple1))
+
+boolean = True
+print("this is boole ->", boolean)
+
+float = 0.8
+print("this is float -> " + str(float))
+
+lst = ["f", "f", "ds", "ds"]
+print ("this is list ->", str(lst))
